5ec851557beb9207cdbe15dab73d69551d4824b7
Pavel Bazhin <p81181@yandex.ru>
2023-03-23 21:04:49 +0300

    Sqr

---
+++ ZADACHA_1/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Enter number");
+string strNum = Console.ReadLine();
+int num = Convert.ToInt32(strNum);
+//int sqr = num * num;
+double sqr = Math.Pow(num,2);
+Console.WriteLine($"sqr = {sqr}");
