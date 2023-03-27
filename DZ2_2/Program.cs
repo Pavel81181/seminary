d4e2f4cff444a830b86fb289f36fc631b9bb5b9c
Pavel Bazhin <p81181@yandex.ru>
2023-03-27 22:58:53 +0300

    DZ2_1,2

---
+++ DZ2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа для обзначения третьей цифры числа
+Console.Clear();
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine("3 цифры нет");
+}
+else
+{
+    while (num > 999)
+    {
+        num = num/10;
+    }
+int num2 = num%10;
+Console.WriteLine(num2);
+}
